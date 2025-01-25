35791d91cdcd60e1d05d3c67e653c304ab0947bd
heygideon <47083178+heygideon@users.noreply.github.com>
2025-01-25 17:38:44 +0000

    fix: remove hono client

--- api/src/client.ts
+++ api/src/client.ts
@@ -1,30 +1,6 @@
-import { hc } from "hono/client";
-import type { App } from ".";
-
 import type { Schema } from "./db";
 
-const client = hc<App>("http://localhost:3000");
-export default client;
-
-export const getAccessToken = async () => {
-  let token = localStorage.getItem("access_token");
-  if (!token) {
-    throw new Error("No access token found");
-  }
-
-  const [, payload] = token.split(".");
-  const decoded = JSON.parse(atob(payload));
-  if (decoded.exp * 1000 < Date.now()) {
-    const refreshToken = localStorage.getItem("refresh_token");
-    if (!refreshToken) {
-      throw new Error("No refresh token found");
-    }
-
-    // TODO: refresh token
-  }
-
-  return token;
-};
+export type { AppRouter } from ".";
 
 export type Table<
   K extends keyof Schema,

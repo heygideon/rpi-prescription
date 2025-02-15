841d562b87e4687bd3f79105fcbe8628c363aeca
heygideon <47083178+heygideon@users.noreply.github.com>
2025-02-15 21:31:54 +0000

    fix: update prescription design

--- app/app/routes/prescription/view.tsx
+++ app/app/routes/prescription/view.tsx
@@ -17,7 +17,7 @@ export default function PrescriptionView({ params }: Route.ComponentProps) {
 
   return (
     <>
-      <div className="sticky top-0 bg-white p-6 shadow-md">
+      <div className="-mb-6 bg-gray-100 p-6 pb-12">
         <button
           onClick={() => navigate(-1)}
           className="-m-2 grid p-2 transition active:scale-95 active:opacity-75"
@@ -39,135 +39,152 @@ export default function PrescriptionView({ params }: Route.ComponentProps) {
           <>
             <div className="mt-2.5 h-8 w-16 animate-pulse rounded bg-gray-300"></div>
             <div className="mt-1 h-5 w-28 animate-pulse rounded bg-gray-300"></div>
+            <div className="h-px"></div>
+            <div className="mt-4 pb-6">
+              <div className="flex items-center">
+                <div className="h-0.5 w-8 flex-none overflow-clip rounded-r-full bg-gray-300"></div>
+                <div className="size-10 rounded-full border-2 border-gray-300 text-gray-300"></div>
+                <div className="h-0.5 min-w-0 flex-1 overflow-clip bg-gray-300"></div>
+                <div className="size-10 rounded-full border-2 border-gray-300 text-gray-300"></div>
+                <div className="h-0.5 min-w-0 flex-1 overflow-clip bg-gray-300"></div>
+                <div className="size-10 rounded-full border-2 border-gray-300 text-gray-300"></div>
+                <div className="h-0.5 min-w-0 flex-1 overflow-clip bg-gray-300"></div>
+                <div className="size-10 rounded-full border-2 border-gray-300 text-gray-300"></div>
+                <div className="h-0.5 w-8 flex-none overflow-clip rounded-r-full bg-gray-300"></div>
+              </div>
+            </div>
           </>
         )}
       </div>
-      <div className="space-y-6 p-6 pb-24">
-        {!!order && (
-          <>
-            <section>
-              <h2 className="text-xl font-bold tracking-tight">
-                Your pharmacy
-              </h2>
-              <div className="mt-3 rounded-md border border-gray-300 bg-white p-4 shadow-sm transition active:scale-95 active:opacity-75">
-                <h3 className="text-lg font-bold tracking-tight">
-                  Cohens Chemist
-                </h3>
-                <p className="truncate text-sm text-gray-600">
-                  4 Privet Drive, Little Whinging, Surrey GU1 3SX
-                </p>
-                <div className="mt-1.5 flex items-baseline">
-                  <span className="text-sm font-semibold text-emerald-700">
-                    More info
-                  </span>
-                  <div className="flex-1"></div>
-                  <span className="text-xs text-gray-600">
-                    open today until 6pm
-                  </span>
+      <div className="relative isolate min-h-64 overflow-clip rounded-t-xl border-t border-gray-200 bg-gray-100 p-6 pb-24">
+        <div className="absolute inset-x-0 top-0 -z-10 h-64 bg-gradient-to-b from-white to-gray-100"></div>
+        <div className="space-y-6">
+          {!!order && (
+            <>
+              <section>
+                <h2 className="text-xl font-bold tracking-tight">
+                  Your pharmacy
+                </h2>
+                <div className="mt-3 rounded-md border border-gray-300 bg-white p-4 shadow-sm transition active:scale-95 active:opacity-75">
+                  <h3 className="text-lg font-bold tracking-tight">
+                    Cohens Chemist
+                  </h3>
+                  <p className="truncate text-sm text-gray-600">
+                    4 Privet Drive, Little Whinging, Surrey GU1 3SX
+                  </p>
+                  <div className="mt-1.5 flex items-baseline">
+                    <span className="text-sm font-semibold text-emerald-700">
+                      More info
+                    </span>
+                    <div className="flex-1"></div>
+                    <span className="text-xs text-gray-600">
+                      open today until 6pm
+                    </span>
+                  </div>
                 </div>
-              </div>
-            </section>
-            <section>
-              <h2 className="text-xl font-bold tracking-tight">
-                Order details
-              </h2>
-              <div className="mt-3 space-y-3">
-                <div className="relative flex gap-3 rounded-md border border-gray-300 bg-white p-2 shadow-sm transition active:scale-95 active:opacity-75">
-                  <span className="absolute left-2 top-2 grid size-8 place-items-center rounded-full bg-gray-300 font-semibold leading-none">
-                    2
-                  </span>
-                  <img
-                    src={paracetamolSrc}
-                    alt="Paracetamol"
-                    className="size-24"
-                  />
-                  <div className="flex min-w-0 flex-1 flex-col justify-center">
-                    <h3 className="truncate text-lg font-bold tracking-tight">
-                      Paracetamol
-                    </h3>
-                    <p className="truncate text-sm text-gray-600">
-                      500mg capsules
-                    </p>
-                    <div className="mt-1.5 flex items-baseline">
-                      <span className="text-sm font-semibold text-emerald-700">
-                        Dosage info
-                      </span>
+              </section>
+              <section>
+                <h2 className="text-xl font-bold tracking-tight">
+                  Order details
+                </h2>
+                <div className="mt-3 space-y-3">
+                  <div className="relative flex gap-3 rounded-md border border-gray-300 bg-white p-2 shadow-sm transition active:scale-95 active:opacity-75">
+                    <span className="absolute left-2 top-2 grid size-8 place-items-center rounded-full bg-gray-300 font-semibold leading-none">
+                      2
+                    </span>
+                    <img
+                      src={paracetamolSrc}
+                      alt="Paracetamol"
+                      className="size-24"
+                    />
+                    <div className="flex min-w-0 flex-1 flex-col justify-center">
+                      <h3 className="truncate text-lg font-bold tracking-tight">
+                        Paracetamol
+                      </h3>
+                      <p className="truncate text-sm text-gray-600">
+                        500mg capsules
+                      </p>
+                      <div className="mt-1.5 flex items-baseline">
+                        <span className="text-sm font-semibold text-emerald-700">
+                          Dosage info
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="relative flex gap-3 rounded-md border border-gray-300 bg-white p-2 shadow-sm transition active:scale-95 active:opacity-75">
+                    <span className="absolute left-2 top-2 grid size-8 place-items-center rounded-full bg-gray-300 font-semibold leading-none">
+                      2
+                    </span>
+                    <img
+                      src={paracetamolSrc}
+                      alt="Paracetamol"
+                      className="size-24"
+                    />
+                    <div className="flex min-w-0 flex-1 flex-col justify-center">
+                      <h3 className="truncate text-lg font-bold tracking-tight">
+                        Paracetamol
+                      </h3>
+                      <p className="truncate text-sm text-gray-600">
+                        500mg capsules
+                      </p>
+                      <div className="mt-1.5 flex items-baseline">
+                        <span className="text-sm font-semibold text-emerald-700">
+                          Dosage info
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="relative flex gap-3 rounded-md border border-gray-300 bg-white p-2 shadow-sm transition active:scale-95 active:opacity-75">
+                    <span className="absolute left-2 top-2 grid size-8 place-items-center rounded-full bg-gray-300 font-semibold leading-none">
+                      2
+                    </span>
+                    <img
+                      src={paracetamolSrc}
+                      alt="Paracetamol"
+                      className="size-24"
+                    />
+                    <div className="flex min-w-0 flex-1 flex-col justify-center">
+                      <h3 className="truncate text-lg font-bold tracking-tight">
+                        Paracetamol
+                      </h3>
+                      <p className="truncate text-sm text-gray-600">
+                        500mg capsules
+                      </p>
+                      <div className="mt-1.5 flex items-baseline">
+                        <span className="text-sm font-semibold text-emerald-700">
+                          Dosage info
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="relative flex gap-3 rounded-md border border-gray-300 bg-white p-2 shadow-sm transition active:scale-95 active:opacity-75">
+                    <span className="absolute left-2 top-2 grid size-8 place-items-center rounded-full bg-gray-300 font-semibold leading-none">
+                      2
+                    </span>
+                    <img
+                      src={paracetamolSrc}
+                      alt="Paracetamol"
+                      className="size-24"
+                    />
+                    <div className="flex min-w-0 flex-1 flex-col justify-center">
+                      <h3 className="truncate text-lg font-bold tracking-tight">
+                        Paracetamol
+                      </h3>
+                      <p className="truncate text-sm text-gray-600">
+                        500mg capsules
+                      </p>
+                      <div className="mt-1.5 flex items-baseline">
+                        <span className="text-sm font-semibold text-emerald-700">
+                          Dosage info
+                        </span>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="relative flex gap-3 rounded-md border border-gray-300 bg-white p-2 shadow-sm transition active:scale-95 active:opacity-75">
-                  <span className="absolute left-2 top-2 grid size-8 place-items-center rounded-full bg-gray-300 font-semibold leading-none">
-                    2
-                  </span>
-                  <img
-                    src={paracetamolSrc}
-                    alt="Paracetamol"
-                    className="size-24"
-                  />
-                  <div className="flex min-w-0 flex-1 flex-col justify-center">
-                    <h3 className="truncate text-lg font-bold tracking-tight">
-                      Paracetamol
-                    </h3>
-                    <p className="truncate text-sm text-gray-600">
-                      500mg capsules
-                    </p>
-                    <div className="mt-1.5 flex items-baseline">
-                      <span className="text-sm font-semibold text-emerald-700">
-                        Dosage info
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="relative flex gap-3 rounded-md border border-gray-300 bg-white p-2 shadow-sm transition active:scale-95 active:opacity-75">
-                  <span className="absolute left-2 top-2 grid size-8 place-items-center rounded-full bg-gray-300 font-semibold leading-none">
-                    2
-                  </span>
-                  <img
-                    src={paracetamolSrc}
-                    alt="Paracetamol"
-                    className="size-24"
-                  />
-                  <div className="flex min-w-0 flex-1 flex-col justify-center">
-                    <h3 className="truncate text-lg font-bold tracking-tight">
-                      Paracetamol
-                    </h3>
-                    <p className="truncate text-sm text-gray-600">
-                      500mg capsules
-                    </p>
-                    <div className="mt-1.5 flex items-baseline">
-                      <span className="text-sm font-semibold text-emerald-700">
-                        Dosage info
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="relative flex gap-3 rounded-md border border-gray-300 bg-white p-2 shadow-sm transition active:scale-95 active:opacity-75">
-                  <span className="absolute left-2 top-2 grid size-8 place-items-center rounded-full bg-gray-300 font-semibold leading-none">
-                    2
-                  </span>
-                  <img
-                    src={paracetamolSrc}
-                    alt="Paracetamol"
-                    className="size-24"
-                  />
-                  <div className="flex min-w-0 flex-1 flex-col justify-center">
-                    <h3 className="truncate text-lg font-bold tracking-tight">
-                      Paracetamol
-                    </h3>
-                    <p className="truncate text-sm text-gray-600">
-                      500mg capsules
-                    </p>
-                    <div className="mt-1.5 flex items-baseline">
-                      <span className="text-sm font-semibold text-emerald-700">
-                        Dosage info
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-          </>
-        )}
+              </section>
+            </>
+          )}
+        </div>
       </div>
 
       <Transition show={!!order && order.status === "ready"}>

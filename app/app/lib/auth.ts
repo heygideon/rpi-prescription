8e5c8fd62daf3d7616182e4fd87a82b476ca165e
heygideon <47083178+heygideon@users.noreply.github.com>
2025-03-10 21:39:17 +0000

    fix(app): use hosted api endpoint if exists

--- app/app/lib/auth.ts
+++ app/app/lib/auth.ts
@@ -1,3 +1,7 @@
 import Auth from "@repo/auth";
 
-export const auth = new Auth("http://localhost:3000/auth");
+export const getApiUrl = () => {
+  return import.meta.env.RENDER_API_URL || "http://localhost:3000";
+};
+
+export const auth = new Auth(`${getApiUrl()}/auth`);
--- app/app/lib/trpc.ts
+++ app/app/lib/trpc.ts
@@ -1,8 +1,8 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createClient, createQueryUtils } from "@repo/trpc";
-import { auth } from "./auth";
+import { auth, getApiUrl } from "./auth";
 
-export const client = createClient("http://localhost:3000/trpc", { auth });
+export const client = createClient(`${getApiUrl()}/trpc`, { auth });
 export const queryClient = new QueryClient();
 
 export const trpcUtils = createQueryUtils({

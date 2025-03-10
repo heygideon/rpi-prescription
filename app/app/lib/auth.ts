5257892ce07aebaa19b5b135871364d20fdd1f64
heygideon <47083178+heygideon@users.noreply.github.com>
2025-03-10 21:45:38 +0000

    fix(app): hosted api env variable

--- app/app/lib/auth.ts
+++ app/app/lib/auth.ts
@@ -1,7 +1,7 @@
 import Auth from "@repo/auth";
 
 export const getApiUrl = () => {
-  return import.meta.env.RENDER_API_URL || "http://localhost:3000";
+  return import.meta.env.VITE_RENDER_API_URL || "http://localhost:3000";
 };
 
 export const auth = new Auth(`${getApiUrl()}/auth`);

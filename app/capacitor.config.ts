0e40f027aa296ba28afdee1f6a27fd7c2487959a
heygideon <47083178+heygideon@users.noreply.github.com>
2025-03-15 15:27:07 +0000

    refactor: install vite, react plugin (wip)

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.synapse",
   appName: "SuperCoolApp",
-  webDir: "build/client",
+  webDir: "dist",
 
   // for dev only
   android: {

af1cb52113d180a17b935752c832b5d8a8559653
heygideon <47083178+heygideon@users.noreply.github.com>
2025-03-15 20:30:17 +0000

    fix: env variables

--- app/src/lib/auth.ts
+++ app/src/lib/auth.ts
@@ -2,8 +2,9 @@ import Auth from "@repo/auth";
 
 export const getApiUrl = () => {
   try {
-    return import.meta.env.VITE_RENDER_API_URL || "http://localhost:3000";
+    return import.meta.env.PUBLIC_RENDER_API_URL || "http://localhost:3000";
   } catch (e) {
+    // PUBLIC_RENDER_API_URL is not defined
     return "http://localhost:3000";
   }
 };

746acbd4ed615121de6729bd81688d987c310ebd
heygideon <47083178+heygideon@users.noreply.github.com>
2025-03-23 00:42:43 +0000

    feat(admin): query order info from db

--- api/src/routers/admin/orders.ts
+++ api/src/routers/admin/orders.ts
@@ -1,5 +1,7 @@
+import { z } from "zod";
 import db from "../../db";
 import { publicProcedure, router } from "../../lib/trpc";
+import { eq } from "drizzle-orm";
 
 const adminOrdersRouter = router({
   getAll: publicProcedure.query(async () => {
@@ -14,6 +16,20 @@ const adminOrdersRouter = router({
       orderBy: db.orders.id,
     });
   }),
+  getOne: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ input }) => {
+      await new Promise((r) => setTimeout(r, 500));
+
+      return await db.query.orders.findFirst({
+        where: eq(db.orders.id, input.id),
+        with: {
+          user: {
+            columns: { title: true, firstName: true, lastName: true },
+          },
+        },
+      });
+    }),
 });
 
 export default adminOrdersRouter;

d90618cb93dd7a1b940949731913b8510b7f1dde
heygideon <47083178+heygideon@users.noreply.github.com>
2025-03-24 21:19:53 +0000

    fix(admin): add login page background noise

--- admin/src/routes/auth/_layout.tsx
+++ admin/src/routes/auth/_layout.tsx
@@ -2,7 +2,8 @@ import { Outlet } from "react-router";
 
 export default function LoginLayout() {
   return (
-    <div className="h-full overflow-y-auto bg-gradient-to-br from-cyan-600 to-cyan-700 p-12">
+    <div className="relative isolate h-full overflow-y-auto bg-gradient-to-br from-cyan-600 to-cyan-800 p-12">
+      <div className="bg-noise absolute inset-0 -z-10 invert"></div>
       <div className="mx-auto max-w-lg rounded-xl bg-white p-1.5 shadow-lg">
         <div className="rounded-lg border-2 border-dashed border-gray-200 p-6">
           <Outlet />
